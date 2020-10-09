f8e14c6734a122254b3d60287b46e95834d15b78
Emmytobs <eotobo11@gmail.com>
2020-10-10 00:48:20 +0100

    Added middleware; added virtual property
    I added middleware to generate a JWT token which will be called when some user routes are called. Also, i added a toJSON method, which is run everytime res.json() is called to send json data to the client. The method removes the password and tokens array from the user instance before it's sent to the client
    
    I also added a virtual property, Products, to the user schema.

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose  = require('mongoose')
+const jwt = require('jsonwebtoken')
 
-const userModel = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         trim: true,
@@ -19,10 +20,40 @@ const userModel = new mongoose.Schema({
             // write  validation logic
             return value
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
 }, {
     timestamps: true
 })
 
-const User = mongoose.model('User', userModel)
+userSchema.virtual('products', {
+    ref: "Product",
+    localField: "_id",
+    foreignField: "creator"
+})
+
+userSchema.methods.toJSON = function() {
+    const userObject = this.toObject();
+
+    delete userObject.password;
+    delete userObject.tokens;
+
+    return userObject;
+}
+
+userSchema.methods.createJWT = async function() {
+    // "this" refers to the user object
+    const token = jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET)
+    // Add the new token to the beginning of the tokens array
+    this.tokens.unshift({ token });
+    // return back the new token
+    return token;
+}
+
+const User = mongoose.model('User', userSchema)
 module.exports = User;
