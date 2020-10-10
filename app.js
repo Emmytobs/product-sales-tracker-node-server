9f82cc00a16fa342dad48f055b1e5a85a029b0a4
Emmytobs <eotobo11@gmail.com>
2020-10-10 01:00:31 +0100

    Experimenting with Mongoose's query methods

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express');
 const app = express();
+const User = require('./models/user')
 require('dotenv').config({ path: path.resolve(process.cwd(), 'config', '.env') })
 // Mongoose config for the MongoDB database
 require('./mongoose');
@@ -17,6 +18,15 @@ app.use(express.json())
 // Register middleware for the routes
 app.use('/user', userRoutes)
 app.use('/product', productRoutes)
+
+async function main() {
+    const query = await User.find();
+    const selection = query.$where(function() { // query.$where is not a function
+        return this.username === "Emmanuel Otobo"
+    })
+    console.log(selection)
+}
+// main()
 
 app.listen(PORT, () => {
     console.log('Server running on port ', PORT)
